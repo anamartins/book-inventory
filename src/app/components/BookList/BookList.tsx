db94ebc8e520924c2613b0b2ec5ca9dc096a7864
Ana Martins <anacarolina.costamartins@decathlon.com>
2024-09-11 17:43:09 +0200

    add id as key in BookList

--- src/app/components/BookList/BookList.tsx
+++ src/app/components/BookList/BookList.tsx
@@ -4,7 +4,7 @@ import './BookList.scss';
 const BookList = ({books}) => {
     const bookList = books.length ? books.map( (book, index:number) => {
         const slug = "/books/" + book.slug
-        return <li className="book-list-item" key={book.name}>
+        return <li className="book-list-item" key={book.id}>
             <h2>{index + 1}. <a href={slug}>{book.name}</a>, {book.authorName}</h2>
             <p>Publisher: {book.publisher}, {book.year}. {book.edition} edition.</p> 
             
